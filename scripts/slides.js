2871c007e6865dcdc913025f8c9b516c8bc118de
giantspacemonster <meatonplate@gmail.com>
2020-10-04 19:23:46 +0530

    Added style sheet default for home page

--- scripts/slides.js
+++ scripts/slides.js
@@ -1,4 +1,4 @@
-var slideIndex = 1;
+var slideIndex=1;
 showSlides(slideIndex);
 
 var slideIndex2 = 0;
@@ -9,15 +9,15 @@ function plusSlides(n) {
 }
 
 function currentSlide(n) {
-    showSLides(slideIndex = n);
+    showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElemenstByCalssName("dot");
+    var dots = document.getElementsByClassName("dot");
     if(n > slides.length){
-	slidesIndex = 1;
+	slideIndex = 1;
     }
     if(n < 1) {
 	slideIndex = slides.length;
@@ -28,7 +28,7 @@ function showSlides(n) {
     for(i = 0; i < dots.length; i++) {
 	dots[i].className = dots[i].className.replace(" active", "");
     }
-    slides[slideIndex - 1].style.display = "block";
+    slides[slideIndex-1].style.display = "block";
     dots[slideIndex - 1].className += " active";
 }
 
@@ -39,14 +39,14 @@ function showSlides2() {
     for(i = 0; i< slides.length; i++) {
 	slides[i].style.display = "none";
     }
-    slideindex2++;
+    slideIndex2++;
     if(slideIndex2 > slides.length) {
-	slideIndex = 1;
+	slideIndex2 = 1;
     }
     for( i = 0; i<dots.length; i++) {
 	dots[i].className = dots[i].className.replace(" active", "");
     }
     slides[slideIndex2 - 1].style.display = "block";
-    dots[slideindex2 - 1].className += " active";
-    settimeout(showSlides2, 4000); //change image every 2 seconds.
+    dots[slideIndex2 - 1].className += " active";
+    setTimeout(showSlides2, 4000); //change image every 2 seconds.
 }
