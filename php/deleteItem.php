38ba7f14e6d5f59ca267a10dd467000bc7be506d
giantspacemonster <meatonplate@gmail.com>
2020-10-14 12:57:49 +0530

    Fixed cart numbering error; style improvements;

--- php/deleteItem.php
+++ php/deleteItem.php
@@ -3,22 +3,32 @@
   $conn = pg_connect("host=localhost port=5432 dbname=farmhousefreshdb user=farmhousefreshdb password=secretpassword") or ("Could not establish database connection!!!");
   
   $_SESSION['cartitem'] = $_GET['cartitem'];
-  //echo $_SESSION['cartvalue'];
   
   $queryCart="DELETE FROM cart WHERE sr_no='".$_SESSION['cartitem']."';";
-  //echo $queryCart;
   $res = pg_query($queryCart);
   
   $queryCount = "SELECT * FROM cart;";
   $resCount = pg_query($queryCount);
   
-  $count = $_SESSION['cartitem'];
+  $count = pg_num_rows($resCount);
 
-  while($cart=pg_fetch_array($resCount)){
-      $queryCart = "UPDATE cart SET sr_no=".++$count." WHERE sr_no >= '".$_SESSION['cartitem']++."';";
+  for($i = 1; $i <= $count; $i++){
+    if($i < $_SESSION['cartitem']){
+      echo "Do Nothing";
+    }
+    if($i >= $_SESSION['cartitem']){
+      $queryCart = "UPDATE cart SET sr_no='".$i."' WHERE sr_no = '".++$_SESSION['cartitem']."';";
+      //echo $queryCart;
       $resCart = pg_query($queryCart);
+    }
+  }
+  header('location: ./cart.php');
+
+
+/*  while($cart=pg_fetch_array($resCount)){
+   
       header('location: ./cart.php');
-      /* ERROR IN OPERATION:
+       * ERROR IN OPERATION:
        * Deletion of last cart item is an error.
        * Deletion of middle items(with atleast one item above and atleast one item below)
        * assigns wrong sr_no to cart items.
@@ -26,8 +36,7 @@
        * For Deletion of middle items, an if condition, with if $count greater than the $cart['sr_no'] then <condition>
        * and if $count less than $cart['sr_no'] <condition>
        * END OF ERROR REPORT
-      */
   }
+*/  
   echo pg_result_error($resCart);
-  echo 'Hello';
 ?>
